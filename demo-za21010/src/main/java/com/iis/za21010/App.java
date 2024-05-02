0638e46cb532eedfc0565812ce743f2d63f1f2ff
andresdevav <andres000alvarado@gmail.com>
2024-05-01 19:23:05 -0600

    My firts commit

---
+++ demo-za21010/src/main/java/com/iis/za21010/App.java
@@ -0,0 +1,17 @@
+package com.iis.za21010;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public String Hello() {
+		return "Hello world";
+	}
+    public static void main( String[] args )
+    {
+    	App hw = new App();
+        System.out.println(hw.Hello());
+    }
+}
